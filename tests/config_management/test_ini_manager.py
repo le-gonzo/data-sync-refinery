58c2b189cd3d6141621604bad59b4ba76425a5b1
Luis E. Gonzalez <lgonzal1@hs.uci.edu>
2023-09-27 12:46:09 -0700

    Updated unit test for ini_manager and renamed secrets.ini to secrets.ini.template

--- tests/config_management/test_ini_manager.py
+++ tests/config_management/test_ini_manager.py
@@ -1,39 +1,31 @@
 import unittest
-from unittest.mock import patch, Mock, mock_open
-# Adjusted import based on your directory structure
+from unittest.mock import patch, mock_open
 from config_management.ini_manager import INISecretManager
+import configparser
 
 class TestINISecretManager(unittest.TestCase):
 
     def setUp(self):
+        # Create a real configparser object that doesn't read from the file system.
+        self.mock_config = configparser.ConfigParser()
         self.ini_manager = INISecretManager()
+        self.ini_manager.config = self.mock_config  # Replace the instance's config with our mock one
 
-    @patch('config_management.ini_manager.configparser.ConfigParser.read')
-    @patch('config_management.ini_manager.configparser.ConfigParser.__getitem__')
-    def test_get_secret_valid(self, mock_getitem, mock_read):
-        # Mocking secrets.ini contents
-        mock_getitem.return_value = {'KEY1': 'VALUE1'}
-        
+    def test_get_secret_valid(self):
+        self.mock_config.read_string('[Secrets]\nKEY1=VALUE1\n')
         secret = self.ini_manager.get_secret('KEY1')
         self.assertEqual(secret, 'VALUE1')
 
-    @patch('config_management.ini_manager.configparser.ConfigParser.read')
-    @patch('config_management.ini_manager.configparser.ConfigParser.__getitem__')
-    def test_get_secret_invalid(self, mock_getitem, mock_read):
-        # Mocking secrets.ini contents without the desired key
-        mock_getitem.side_effect = KeyError
-
+    def test_get_secret_invalid(self):
         with self.assertRaises(ValueError):
             self.ini_manager.get_secret('INVALID_KEY')
 
-    @patch('config_management.ini_manager.configparser.ConfigParser.read')
-    @patch('config_management.ini_manager.configparser.ConfigParser.__setitem__')
     @patch('config_management.ini_manager.open', new_callable=mock_open)
-    def test_set_secret(self, mock_file_open, mock_setitem, mock_read):
+    def test_set_secret(self, mock_file_open):
         self.ini_manager.set_secret('NEW_KEY', 'NEW_VALUE')
         
         # Check if the correct value is set
-        mock_setitem.assert_called_with('Secrets', {'NEW_KEY': 'NEW_VALUE'})
+        self.assertEqual(self.mock_config['Secrets']['NEW_KEY'], 'NEW_VALUE')
 
         # Check if file write has been triggered
         mock_file_open.assert_called_with('../secrets.ini', 'w')
